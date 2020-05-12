a98a2c418cfc4f56188b20ea188c45887b06ef85
Gitea <gitea@fake.local>
2020-05-12 16:08:24 +0300

    Finished Lab 12

---
+++ RazerPagesPC/Models/CommentDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using RazerPagesPC.Models;
+
+namespace RazerPagesPC.Models
+{
+    public class CommentDTO
+    {
+        [DataMember]
+        public int CommentId { get; set; }
+        [DataMember]
+        public string Text { get; set; }
+        [DataMember]
+        public int PostPostId { get; set; }
+        [DataMember]
+        public virtual PostDTO PostDTO { get; set; }
+    }
+}
